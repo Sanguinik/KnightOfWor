1524b9a381fe716e2a6bed2176e5a35b51d0ae57
Hobbit <Hobbit@192.168.0.100>
2012-08-20 21:57:31 +0200

    Added first model

---
+++ KnightOfWor/src/main/java/com/wordpress/marleneknoche/model/Enemy.java
@@ -0,0 +1,33 @@
+package com.wordpress.marleneknoche.model;
+
+public class Enemy {
+	
+	private int points;
+	private Bullet bullet;
+	private Boolean alive;
+	
+	public Enemy(){
+		
+	}
+	
+	public int getPoints(){
+		return points;
+	}
+	
+	public void shoot(){
+		
+	}
+	
+	public Boolean isAlive(){
+		return alive;
+	}
+	
+	public void setAlive(Boolean alive){
+		this.alive=alive;
+	}
+	
+	public void move(){
+		
+	}
+
+}
---
+++ KnightOfWor/src/main/java/com/wordpress/marleneknoche/model/Player.java
@@ -0,0 +1,55 @@
+package com.wordpress.marleneknoche.model;
+
+public class Player {
+	
+	private int points = 1000;
+	private int score = 0;
+	private Boolean alive = true;
+	private Bullet bullet;
+	private int lives = 4;
+	private final int maxLives = 4;
+	
+	public int getPoints(){
+		return points;
+	}
+	
+	public int getScore(){
+		return score;
+	}
+	
+	public void setScore(int score){
+		this.score=score;
+	}
+	
+	public Boolean isAlive(){
+		return alive;
+	}
+	
+	public void setAlive(Boolean alive){
+		this.alive=alive;
+	}
+	
+	public void shoot(){
+		
+	}
+	
+	public int getLives(){
+		return lives;
+	}
+	
+	public void setLives(int lives){
+		this.lives=lives;
+		if(lives>maxLives){
+			this.lives=maxLives;
+		}
+	}
+	
+	public int getMaxLives(){
+		return maxLives;
+	}
+	
+	public void move(){
+		
+	}
+
+}
