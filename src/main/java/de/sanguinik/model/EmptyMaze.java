6d2c6ab5eb91941f6d9f898edc4a5cce0952bed2
Marlene Knoche <marlene.knoche@saxsys.de>
2015-09-30 18:45:09 +0200

    add maze is created by JSON file

--- src/main/java/de/sanguinik/model/EmptyMaze.java
+++ src/main/java/de/sanguinik/model/EmptyMaze.java
@@ -1,11 +1,18 @@
 package de.sanguinik.model;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public abstract class EmptyMaze{
 
@@ -16,13 +23,14 @@ public abstract class EmptyMaze{
 	}
 
 	private void createMaze() {
+		
+		double[][] values = loadJSON();
+		
 		Group g = new Group();
 
-		// outer walls
-		walls.add(new Rectangle(112, 70, 800, 10));
-		walls.add(new Rectangle(112, 563, 800, 10));
-		walls.add(new Rectangle(112, 70, 10, 493));
-		walls.add(new Rectangle(902, 70, 10, 493));
+		for(int i = 0; i<values.length; i++){
+			walls.add(new Rectangle(values[i][0], values[i][1], values[i][2], values[i][3]));
+		}
 
 		for (Rectangle r : walls) {
 			r.setFill(Color.BLUE);
@@ -37,6 +45,48 @@ public abstract class EmptyMaze{
 	
 	public void addWalls(List<Rectangle> walls){
 		this.walls.addAll(walls);
+	}
+	
+	private double[][] loadJSON(){
+		
+		double[][] result = null;
+		
+		JSONParser parser = new JSONParser();
+		
+		try{
+			
+			Object obj = parser.parse(new FileReader("./src/main/resources/de/sanguinik/model/level1.json"));
+			JSONObject jsonObject = (JSONObject) obj;
+
+			double[][] blockValues = new double[jsonObject.size()][4];
+			
+			for(int i = 1; i <= jsonObject.size(); i++){
+				JSONObject block = (JSONObject) jsonObject.get("Block"+i);
+				
+				long x = (Long) block.get("x");
+				double dx = (double) x;
+				blockValues[i-1][0] = dx;
+
+				long y = (Long) block.get("y");
+				double dy = (double) y;
+				blockValues[i-1][1] = dy;
+				
+				long width = (Long) block.get("width");
+				double dwidth = (double) width;
+				blockValues[i-1][2] = dwidth;
+				
+				long height = (Long) block.get("height");
+				double dheight = (double) height;
+				blockValues[i-1][3] = dheight;
+
+			}
+			
+			result = blockValues;
+			
+		} catch (IOException | ParseException e){
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 }
--- src/main/java/de/sanguinik/model/Maze.java
+++ src/main/java/de/sanguinik/model/Maze.java
@@ -24,57 +24,7 @@ public class Maze extends EmptyMaze{
 	}
 
 	private void createMaze() {
-//		Group g = new Group();
-
-		//walls.add(new Rectangle(x,y,l,h);
-		// outer walls
-//		walls.add(new Rectangle(112, 70, 800, 10));
-//		walls.add(new Rectangle(112, 563, 800, 10));
-//		walls.add(new Rectangle(112, 70, 10, 493));
-//		walls.add(new Rectangle(902, 70, 10, 493));
-		// inner walls first line - 72 | 83
-		walls.add(new Rectangle(400, 70, 10, 83));
-		walls.add(new Rectangle(616, 70, 10, 83));
-		// inner walls second line -
-		walls.add(new Rectangle(184, 153, 144, 10));
-		walls.add(new Rectangle(688, 153, 144, 10));
-		// third line |
-		walls.add(new Rectangle(184, 153, 10, 83));
-		walls.add(new Rectangle(472, 153, 10, 83));
-		walls.add(new Rectangle(544, 153, 10, 83));
-		walls.add(new Rectangle(832, 153, 10, 83));
-		// fourth line -
-		walls.add(new Rectangle(256, 236, 144, 10));
-		walls.add(new Rectangle(616, 236, 144, 10));
-		// fifth line |
-		walls.add(new Rectangle(256, 236, 10, 166));
-		walls.add(new Rectangle(760, 236, 10, 166));
-		// sixth line -
-		walls.add(new Rectangle(112, 319, 72, 10));
-		walls.add(new Rectangle(400, 319, 72, 10));
-		walls.add(new Rectangle(544, 319, 72, 10));
-		walls.add(new Rectangle(832, 319, 72, 10));
-		// seventh line |
-		walls.add(new Rectangle(328, 319, 10, 83));
-		walls.add(new Rectangle(688, 319, 10, 83));
-		// eighth line -
-		walls.add(new Rectangle(400, 402, 216, 10));
-		// ninth line |
-		walls.add(new Rectangle(184, 402, 10, 83));
-		walls.add(new Rectangle(832, 402, 10, 83));
-		// tenth line -
-		walls.add(new Rectangle(400, 485, 72, 10));
-		walls.add(new Rectangle(544, 485, 72, 10));
-		// eleventh line |
-		walls.add(new Rectangle(328, 485, 10, 83));
-		walls.add(new Rectangle(688, 485, 10, 83));
-
-		for (Rectangle r : walls) {
-			r.setFill(Color.BLUE);
-			
-		}
 		super.getWalls().addAll(walls);
-//		g.getChildren().addAll(walls);
 	}
 
 	public List<Rectangle> getWalls() {
